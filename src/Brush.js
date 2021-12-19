ca70d21221e759cd47078e4587a0071ce76b2694
Samuel <samlee1097@yahoo.com>
2021-12-19 01:13:58 -0500

    finished

--- src/Brush.js
+++ src/Brush.js
@@ -5,7 +5,7 @@ function Brush({handleUtensil}) {
     return (
         <>
             <select onChange={(event)=>handleUtensil(event.target.value, "weight")} id="brush-detail">
-                <option value="3">Thin</option>
+                <option value="2">Thin</option>
                 <option value="5">Normal</option>
                 <option value="9">Thick</option>
             </select>

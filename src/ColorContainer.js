1281db134e2652894372adbe13504af9a29e6c92
Samuel <samlee1097@yahoo.com>
2021-12-21 01:37:35 -0500

    included current color & tool

--- src/ColorContainer.js
+++ src/ColorContainer.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ColorCard from './ColorCard'
+import brush from './pictures/pencil.png'
+import eraser from './pictures/eraser.png'
+import bucket from './pictures/bucket.png'
 import './Header.css'
 
 const colorList = ["red", "blue", "green", "yellow", "pink", "black", "white", "brown", "purple", "grey", "orange"];
@@ -7,15 +10,19 @@ const colorList = ["red", "blue", "green", "yellow", "pink", "black", "white", "
 function ColorContainer({handleUtensil, utensil}) {
     
     const colorGrid = colorList.map((color) => <ColorCard handleUtensil={handleUtensil} key={color} color={color}/>)
+
     return (
         <>
         <div id="color-grid">
             {colorGrid}
         </div>
         <div className='selected-box'>
-            <p className="selected-color">Selected Color
+            <p className="selected-color">Color
                 <div style={{backgroundColor:`${utensil.color}`}}>
                 </div>
+            </p>
+            <p className="selected-tool">Tool
+                <img src={utensil.tool === "brush" ? brush : utensil.tool === "eraser" ? eraser : bucket} style={{width:"12px", margin:"0 2px"}} alt="brush"/>
             </p>
         </div>
         </>
--- src/Tools.js
+++ src/Tools.js
@@ -8,10 +8,10 @@ import './Header.css';
 function Tools({handleUtensil}) {
 
     return (
-        <div style={{backgroundColor:'lightgrey', padding:'5px', width:'140px', textAlign:"center"}}>
-            <button style={{width:"33px"}} name="brush" className="tool-icon" onClick={()=>handleUtensil("brush", "tool")}><img src={brush} style={{width:"12px"}} alt="brush"/></button>
+        <div style={{backgroundColor:'lightgrey', padding:'5px', width:'180px', textAlign:"center"}}>
+            <button style={{width:"33px", margin:"0 2px"}} name="brush" className="tool-icon" onClick={()=>handleUtensil("brush", "tool")}><img src={brush} style={{width:"12px", margin:"0 2px"}} alt="brush"/></button>
             <button style={{width:"33px"}} name="bucket" className="tool-icon" onClick={()=>handleUtensil("bucket", "tool")}><img src={bucket} style={{width:"12px"}} alt="bucket"/></button>
-            <button style={{width:"33px"}} name="eraser" className="tool-icon" onClick={()=>handleUtensil("eraser", "tool")}><img src={eraser} style={{width:"12px"}} alt="eraser"/></button>
+            <button style={{width:"33px", margin:"0 2px"}} name="eraser" className="tool-icon" onClick={()=>handleUtensil("eraser", "tool")}><img src={eraser} style={{width:"12px"}} alt="eraser"/></button>
         </div>
     );
 }
